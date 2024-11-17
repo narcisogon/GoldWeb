7ff1376499ac07aec8017ca737e8b38f2199857e
ExoticExo <codylux1908@gmail.com>
2024-11-16 19:51:32 -0600

    w

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
     <header className="header">
-      <h1>Goldman Sachs Financial Services</h1>
-      <nav>
-        <a href="#banking">Banking</a>
-        <a href="#payments">Payments</a>
-        <a href="#investments">Investments</a>
-        <a href="#fraud-detection">Fraud Detection</a>
-      </nav>
+      <div className="header-container">
+        {/* Clickable Logo */}
+        <a href="/" className="logo">
+          <img 
+            src="https://banner2.cleanpng.com/20180815/pqr/9957df2ba6f55774872856d3fa256e02.webp" 
+            alt="Goldman Sachs Logo" 
+          />
+        </a>
+
+        {/* Navigation Links */}
+        <nav className="nav-links">
+          <a href="#banking">Banking</a>
+          <a href="#payments">Payments</a>
+          <a href="#investments">Investments</a>
+          <a href="#fraud-detection">Fraud Detection</a>
+        </nav>
+
+        {/* Transparent Sign In Button with Icon */}
+        <div className="action-button">
+          <button>
+            <i className="fas fa-user"></i> Sign In
+          </button>
+        </div>
+      </div>
     </header>
   );
 }
 
 export default Header;
+
